5c90691109196cd16fd25c07f7675959d99f953b
bada.kang <kangbada0728@gmail.com>
2022-07-17 18:08:07 +0900

    3.3 람다 활용 : 실행 어라운드 패턴

---
+++ src/test/java/modernJavaInAction/part3_3/BufferedReaderProcessor.java
@@ -0,0 +1,9 @@
+package modernJavaInAction.part3_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface BufferedReaderProcessor {
+    String process(BufferedReader b) throws IOException;
+}
---
+++ src/test/java/modernJavaInAction/part3_3/Main.java
@@ -0,0 +1,22 @@
+package modernJavaInAction.part3_3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        String oneLine = main.processFile(BufferedReader::readLine);
+        System.out.println("oneLine = " + oneLine);
+        String twoLine = main.processFile(b -> b.readLine() + b.readLine());
+        System.out.println("twoLine = " + twoLine);
+    }
+
+    public String processFile(BufferedReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            return p.process(br);
+        }
+    }
+}
