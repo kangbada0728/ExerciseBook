7c7b5c41cfb6f0ba8757e094fafd121a8e4cb605
bada.kang <kangbada0728@gmail.com>
2022-07-17 16:59:14 +0900

    3.7 람다, 메서드 참조 활용하기

--- src/test/java/modernJavaInAction/part3_7/Main.java
+++ src/test/java/modernJavaInAction/part3_7/Main.java
@@ -4,8 +4,11 @@ import modernJavaInAction.quiz2_1.Apple;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class Main {
 
@@ -16,8 +19,16 @@ public class Main {
         Apple a2 = new Apple(2, "green");
         Apple a3 = new Apple(3, "blue");
 
-        List<Apple> list = List.of(a2, a1, a3);
-
+        List<Apple> list = new ArrayList<>(List.of(a2, a1, a3));
+        list.sort(new AppleComparator());
+        list.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        });
+        list.sort(comparing(apple -> apple.getWeight()));
+        list.sort(comparing(Apple::getWeight));
     }
 
     class AppleComparator implements Comparator<Apple> {
