c3feada4d5a78a7d976c8aad65640ad3eac01c90
bada.kang <kangbada0728@gmail.com>
2022-07-19 21:34:54 +0900

    5.2.1 프레디케이트를 이용한 슬라이싱

---
+++ src/test/java/modernJavaInAction/part5/Main.java
@@ -0,0 +1,51 @@
+package modernJavaInAction.part5;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    List<Dish> menu;
+
+    @BeforeEach
+    void beforeEach() {
+        menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+    }
+
+    @Test
+    void test1() {
+        List<Dish> specialMenu = Arrays.asList(
+                new Dish("seasonal fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER));
+
+        specialMenu.stream()
+                .takeWhile(dish -> dish.getCalories() < 320)
+                .forEach(System.out::println);
+
+        specialMenu.stream()
+                .dropWhile(dish -> dish.getCalories() < 320)
+                .forEach(System.out::println);
+    }
+
+
+
+
+
+}
