64e3e157b6344a1e6d302511d8656b00051b454f
bada.kang <kangbada0728@gmail.com>
2022-07-19 20:59:54 +0900

    Quize 4-1 외부 반복 vs 내부 반복

--- src/test/java/modernJavaInAction/part4/Main.java
+++ src/test/java/modernJavaInAction/part4/Main.java
@@ -3,8 +3,7 @@ package modernJavaInAction.part4;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -23,7 +22,6 @@ public class Main {
                 new Dish("pizza", true, 550, Dish.Type.OTHER),
                 new Dish("prawns", false, 300, Dish.Type.FISH),
                 new Dish("salmon", false, 450, Dish.Type.FISH)
-
         );
     }
 
@@ -36,5 +34,29 @@ public class Main {
                 .collect(Collectors.toList());
         System.out.println(threeHighCaloricDishNames);
     }
+
+    @Test
+    void test2() {
+        List<String> highCaloricDishes = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            if (dish.getCalories() > 300) {
+                highCaloricDishes.add(dish.getName());
+            }
+        }
+
+        List<String> highCaloricDishes2 = menu
+                .stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+
+        System.out.println("highCaloricDishes = " + highCaloricDishes);
+        System.out.println("highCaloricDishes2 = " + highCaloricDishes2);
+    }
+
+
+
 
 }
