0e94de8681bc94bf98f327b528402650bb367745
bada.kang <kangbada0728@gmail.com>
2022-07-23 21:01:12 +0900

    [add] 5.7.3 숫자 스트림 활용 : 피타고라스 수

--- src/test/java/modernJavaInAction/part5/Main.java
+++ src/test/java/modernJavaInAction/part5/Main.java
@@ -243,6 +243,24 @@ public class Main {
 
     }
 
+    @Test
+    void test13() {
+
+        Stream<int[]> stream = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                        .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
+        stream.limit(5)
+                .forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        Stream<double[]> stream1 = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)})
+                        .filter(t -> t[2] % 1 == 0));
+    }
+
+
+
 
 
 
