a65500d63af944eb30823ad035335dab43cc03e2
bada.kang <kangbada0728@gmail.com>
2022-07-24 20:47:44 +0900

    add: 6.2 리듀싱과 요약

---
+++ src/test/java/modernJavaInAction/part6/Main.java
@@ -0,0 +1,36 @@
+package modernJavaInAction.part6;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    List<Dish> menu;
+
+    @BeforeEach
+    void beforeEach() {
+        menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+    }
+
+    @Test
+    void test1() {
+        Long howManyDishes = menu.stream().collect(Collectors.counting());
+        long count = menu.stream().count();
+
+
+    }
+}
